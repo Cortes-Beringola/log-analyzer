6442625919ca3cb46f6ec9472d48cc6e51ebb698
Cortes-Beringola <rafa_c94@hotmail.com>
2025-01-29 01:01:34 +0100

    Update test_cli.py

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -13,30 +13,29 @@ def test_incomplete_parameters():  # Raises error
 
 def test_path_does_not_exist():  # Incorrect path raises error
     result = runner.invoke(app, ['/files/wrong_file.log', 'test.json'])
-    file_path = os.getcwd() + '/files/wrong_file.l\nog'
+    file_path = os.getcwd() + '/files/wrong_file.log'
     error_msg = f'[ERROR]{file_path}: {ExitCodes[EXitStatus.NO_EXIST]}'
     assert error_msg in result.stdout
 
 
 def test_incorrect_input_format():  # Test incorrect input format
     result = runner.invoke(app, ['/files/log/short_file.xml', 'test.csv'])
-    file_path = os.getcwd() + '/files/log/short_fi\nle.xml'
-    error_msg = f'[ERROR]{file_path}: Input file does not have correct format. Supported' \
-                ' formats are: json, \ntxt, csv.'
+    file_path = os.getcwd() + '/files/log/short_fi\le.xml'
+    error_msg = f'[ERROR]{file_path}: Input file does not have correct \nformat. Supported formats are: json, txt, csv.'
     assert error_msg in result.stdout
 
 
 def test_no_output_format():  # Test output file with no format
     result = runner.invoke(app, ['/files/log/short_file.log', 'test'])
-    file_path = os.getcwd() + '/files_results/test\n'
-    error_msg = f'[ERROR]{file_path}: {ExitCodes[EXitStatus.NO_OUTPUT_FILE_FORMAT]}'
+    file_path = os.getcwd() + '/files_results/test'
+    error_msg = f'[ERROR]{file_path}: Output file does not have a format. \nSupported formats are: json, txt, csv.'
     assert error_msg in result.stdout
 
 
 def test_incorrect_output_format():  # Test output file with wrong format
     result = runner.invoke(app, ['/files/log/short_file.log', 'test.xml'])
-    file_path = os.getcwd() + '/files_results/test\n.xml'
+    file_path = os.getcwd() + '/files_results/test.xml'
     error_msg = f'[ERROR]{file_path}: Output file does not have correct ' \
-                f'format. Supported formats are: json, \ntxt, csv.'
+                f'\nformat. Supported formats are: json, \ntxt, csv.'
 
     assert error_msg in result.stdout
